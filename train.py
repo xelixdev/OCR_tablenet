49a77f320e8a260286a5de55cc0c3b67eb167a21
Robert Hales <rob.hales@xelix.com>
2022-02-09 22:12:26 +0000

    Fix multiprocessing

--- train.py
+++ train.py
@@ -14,46 +14,47 @@ from pytorch_lightning.callbacks import (
 from tablenet import MarmotDataModule
 from tablenet import TableNetModule
 
-image_size = (896, 896)
-transforms_augmentation = album.Compose(
-    [
-        album.Resize(1024, 1024, always_apply=True),
-        album.RandomResizedCrop(*image_size, scale=(0.7, 1.0), ratio=(0.7, 1)),
-        album.HorizontalFlip(),
-        album.VerticalFlip(),
-        album.Normalize(),
-        ToTensorV2(),
-    ]
-)
+if __name__ == "__main__":
+    image_size = (896, 896)
+    transforms_augmentation = album.Compose(
+        [
+            album.Resize(1024, 1024, always_apply=True),
+            album.RandomResizedCrop(*image_size, scale=(0.7, 1.0), ratio=(0.7, 1)),
+            album.HorizontalFlip(),
+            album.VerticalFlip(),
+            album.Normalize(),
+            ToTensorV2(),
+        ]
+    )
 
-transforms_preprocessing = album.Compose(
-    [album.Resize(*image_size, always_apply=True), album.Normalize(), ToTensorV2()]
-)
+    transforms_preprocessing = album.Compose(
+        [album.Resize(*image_size, always_apply=True), album.Normalize(), ToTensorV2()]
+    )
 
-complaint_dataset = MarmotDataModule(
-    data_dir="./data",
-    transforms_preprocessing=transforms_preprocessing,
-    transforms_augmentation=transforms_augmentation,
-    batch_size=2,
-    num_workers=0,
-)  # type: ignore
+    complaint_dataset = MarmotDataModule(
+        data_dir="./data/xelix_data/images",
+        transforms_preprocessing=transforms_preprocessing,
+        transforms_augmentation=transforms_augmentation,
+        batch_size=2,
+        num_workers=8,
+    )  # type: ignore
 
-model = TableNetModule(batch_norm=False)
+    model = TableNetModule(batch_norm=False)
 
-EXPERIMENT_NAME = f"{model.__class__.__name__}"
-logger = TensorBoardLogger("tb_logs", name=EXPERIMENT_NAME)
+    EXPERIMENT_NAME = f"{model.__class__.__name__}"
+    logger = TensorBoardLogger("tb_logs", name=EXPERIMENT_NAME)
 
-checkpoint_callback = ModelCheckpoint(
-    monitor="validation_loss", save_top_k=5, save_last=True, mode="min"
-)
-early_stop_callback = EarlyStopping(monitor="validation_loss", mode="min", patience=10)
-lr_monitor = LearningRateMonitor(logging_interval="step")
+    checkpoint_callback = ModelCheckpoint(
+        monitor="validation_loss", save_top_k=5, save_last=True, mode="min"
+    )
+    early_stop_callback = EarlyStopping(monitor="validation_loss", mode="min", patience=10)
+    lr_monitor = LearningRateMonitor(logging_interval="step")
 
-trainer = pl.Trainer(
-    callbacks=[lr_monitor, checkpoint_callback, early_stop_callback],
-    logger=logger,
-    max_epochs=500,
-    gpus=1 if torch.cuda.is_available() else None,
-)
-trainer.fit(model, datamodule=complaint_dataset)
-trainer.test()
+    trainer = pl.Trainer(
+        callbacks=[lr_monitor, checkpoint_callback, early_stop_callback],
+        logger=logger,
+        max_epochs=500,
+        gpus=1 if torch.cuda.is_available() else None,
+    )
+    trainer.fit(model, datamodule=complaint_dataset)
+    trainer.test()
